d747a14033236326ba2bd839be75478bcaa398a7
c0a25012e8 <c0a25012e8@edu.teu.ac.jp>
2026-06-09 16:32:00 +0900

    演習問題１

--- pygame_image.py
+++ pygame_image.py
@@ -23,14 +23,17 @@ def main():
 
         key_lst = pg.key.get_pressed()
         
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+        if not key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(-1, 0)
+
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip(1, 0)
+
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
